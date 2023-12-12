745e97ccb74a2f3ef6ba52ccafad47845c6509a5
Oanh Phan <oanhphan@Oanhs-MacBook-Pro.local>
2023-12-12 16:56:06 +0700

    Remove lfs

--- food_analysis/admin.py
+++ food_analysis/admin.py
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f2534ebc0902bd46ac7676cc2dd0a387b7a4eab565ea8a59a5631b6e4427b5dd
-size 265
+from django.contrib import admin
+
+# Register your models here.
+
+from django.contrib import admin
+
+from .models import FoodItem, FoodNutrient, FoodNutrientInItem
+
+admin.site.register(FoodItem)
+admin.site.register(FoodNutrient)
+admin.site.register(FoodNutrientInItem)
