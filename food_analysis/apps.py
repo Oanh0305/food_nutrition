5aec21f317180bb7458377205ac242b166923fb1
Oanh Phan <oanhphan@Oanhs-MacBook-Pro.local>
2023-12-12 16:50:49 +0700

    Remove lfs

--- food_analysis/apps.py
+++ food_analysis/apps.py
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:16d16152fbc8962d510a2bbcc4d050822ec49f2a30997c494dd83c683120b000
-size 157
+from django.apps import AppConfig
+
+
+class FoodAnalysisConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "food_analysis"
